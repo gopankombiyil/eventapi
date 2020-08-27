727b14ef595b9ca0220be8a46955083c797e4dc0
Gopan Kombiyil <gopan@orangewallconsultants.com>
2020-08-27 13:04:49 +1000

    initial commit

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class EventsController extends Controller
+{
+
+    private $events;
+
+    public function __construct()
+    {
+        //import the events json as an array
+        $this->events = json_decode(file_get_contents(storage_path('app/event.json')),true);
+    }
+
+
+    /**
+     * Show all the events
+     * @param  Request $request allow filters to filter results
+     * @return json           json object of events
+     */
+    public function index(Request $request)
+    {
+      $keyword = $request->keyword;
+      $city = $request->city;
+
+      if($request->date) {
+        $eventDate = Carbon::createFromFormat('d/m/Y', $request->date);
+      }
+
+      $result = $this->events;
+
+      //filter the data with the selected keyword if the request contains keyword
+      if($keyword) {
+        $result = array_filter($result, function($el) use ($keyword) {
+          return ( strpos($el['Title'], $keyword) !== false );
+        });
+      }
+
+      //filter the data for city if the requst contains city
+      if($city) {
+        $result = array_filter($result, function($el) use ($city) {
+          return ( $el['Location']['City'] === $city) ;
+        });
+      }
+
+      //filter the data for the date of the request contains a date
+      if(isset($eventDate)) {
+        $result = array_filter($result, function($el) use ($eventDate) {
+          return ( Carbon::parse($el['Time'])->toDateString() === $eventDate->toDateString()) ;
+        });
+      }
+
+      return $result;
+    }
+
+    public function show($id)
+    {
+      return $id;
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .home {
+              width: 100%;
+              height: 100vh;
+              display:block;
+              text-align: center;
+              font-size: 24px;
+              font-weight: 700;
+              padding-top: 3rem;
+              background: rgb(33 29 103 / 11%);
+        </style>
+    </head>
+    <body>
+      <div class="home">API Home</div>
+    </body>
+</html>
